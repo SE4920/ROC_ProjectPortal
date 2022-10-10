e8aef8b977413933f3cf86f3e2ba503a08639ca5
Tristin Rybak <61097414+tristinrybak@users.noreply.github.com>
2022-10-10 18:46:20 -0500

    pls work lol

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   cognito: {
-    userPoolId: 'us-east-1_osxfSZ59t',
-    userPoolWebClientId: '1b3c62pgt5jatrf0pa9m8kg714',
+    userPoolId: 'us-east-2_HdfG1PI9R',
+    userPoolWebClientId: 'g3brsi2a1c9hunok750lv7lhf',
   },
 };
 
