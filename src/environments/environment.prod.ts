7e87a6855c68f00d38564b14c052d24ff524614f
Tristin Rybak <61097414+tristinrybak@users.noreply.github.com>
2022-10-10 19:00:10 -0500

    part 2 homie

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 
   cognito: {
-    userPoolId: 'us-east-1_osxfSZ59t',
-    userPoolWebClientId: '1b3c62pgt5jatrf0pa9m8kg714',
+    userPoolId: 'us-east-2_HdfG1PI9R',
+    userPoolWebClientId: 'g3brsi2a1c9hunok750lv7lhf',
   },
 };
 
