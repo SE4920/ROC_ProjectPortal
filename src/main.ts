f652519dc7c63fd37e326734e2d19810836d4bf9
Lucas-Dowil <89265001+Lucas-Dowil@users.noreply.github.com>
2022-11-03 17:57:21 -0500

    Added API import

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,9 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+import { Amplify, API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+  
+Amplify.configure(awsconfig);
+  
